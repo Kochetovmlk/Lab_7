90080d5d26dcf497a061b2fe15ac17fd23fe43fd
Kochetovmlk <mikhail.kocetov.04@list.ru>
2024-01-08 19:24:12 +0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿using Lab_07.comment;
+using Lab_07.myclass;
+using System.Reflection;
+using System.Xml;
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем новый XML-документ.
+        XmlDocument doc = new XmlDocument();
+        // Создаем корневой элемент "ClassDiagram".
+        XmlElement rootElement = doc.CreateElement("ClassDiagram");
+        // Добавляем корневой элемент к документу.
+        doc.AppendChild(rootElement);
+
+        // Загружаем сборку "Lab_07", содержащую классы с пользовательским атрибутом.
+        Assembly asm = Assembly.Load("Lab_07");
+        // Получаем все типы из загруженной сборки.
+        Type[] types = asm.GetTypes();
+        // Проходимся по каждому типу.
+        foreach (Type t in types)
+        {
+            // Проверяем, принадлежит ли тип пространству имен "Lab_07".
+            if (t.Namespace.Contains("Lab_07"))
+            {
+                // Инициализируем строку "str" значением null.
+                string str = null;
+                // Проверяем, является ли тип классом.
+                if (t.IsClass) { str = "Class"; }
+                // Проверяем, является ли тип перечислением.
+                if (t.IsEnum) { str = "Enum"; }
+                // Создаем XML-элемент с именем, соответствующим типу (Class или Enum).
+                XmlElement element = doc.CreateElement(str);
+                // Добавляем созданный элемент в корневой элемент.
+                rootElement.AppendChild(element);
+                // Устанавливаем атрибут "name" элемента, равным имени типа.
+                element.SetAttribute("name", t.Name);
+
+                // Получаем пользовательский атрибут CommentAttribute для текущего типа.
+                CommentAttribute comment = (CommentAttribute)t.GetCustomAttribute(typeof(CommentAttribute));
+                // Создаем XML-элемент "comment".
+                XmlElement c_element = doc.CreateElement("comment");
+
+                // Проверяем, был ли найден атрибут CommentAttribute.
+                if (comment !=  null)
+                { 
+                    c_element.InnerText = comment.Comment; 
+                    element.AppendChild(c_element); 
+                }
+
+                // Получаем все свойства текущего типа.
+                object[] propiproperties = t.GetProperties();
+                // Проходимся по каждому свойству.
+                foreach (object p in propiproperties)
+                {
+                    // Создаем XML-элемент "properties".
+                    XmlElement p_element = doc.CreateElement("properties");
+                    // Устанавливаем текстовое значение элемента "properties" равным строковому представлению свойства.
+                    p_element.InnerText = p.ToString();
+                    // Добавляем элемент "properties" к текущему элементу (Class или Enum).
+                    element.AppendChild(p_element);
+                }
+
+                // Получаем все методы текущего типа.
+                object[] methods = t.GetMethods(/*BindingFlags.DeclaredOnly*/);
+                // Проходимся по каждому методу.
+                foreach (object m in methods)
+                {
+                    // Создаем XML-элемент "methods".
+                    XmlElement m_element = doc.CreateElement("methods");
+                    // Устанавливаем текстовое значение элемента "methods" равным строковому представлению метода.
+                    m_element.InnerText = m.ToString();
+                    // Добавляем элемент "properties" к текущему элементу (Class или Enum).
+                    element.AppendChild(m_element);
+                }
+            }
+        }
+        doc.Save(Console.Out);
+    }
+}
---
+++ Lab_07/Class1.cs
@@ -0,0 +1,132 @@
+﻿using Lab_07.comment;
+
+namespace Lab_07.myclass
+{
+    [Comment("Class for Cow")]  // Атрибут CommentAttribute для указания комментария к классу.
+    public class Cow : Animal  // Объявление класса Cow, который наследует от абстрактного класса Animal.
+    {
+        // Конструктор класса Cow.
+        public Cow(string Country, string HideFromOtherAnimal, string Name, string WhatAnimal) : base(Country, HideFromOtherAnimal, Name, WhatAnimal)
+        {
+            // Вызов конструктора базового класса Animal для инициализации свойств.
+        }
+
+        // Переопределенный метод SayHello.
+        public override void SayHello()
+        {
+            base.SayHello();  // Вызов метода базового класса.
+            Console.WriteLine(", i'm a Cow.");  // Вывод строки в консоль.
+        }
+
+        // Переопределенный метод GetFavoriteFood.
+        public override eFavoriteFood GetFavoriteFood()
+        {
+            return base.GetFavoriteFood();  // Вызов метода базового класса и возвращение его значения.
+        }
+    }
+
+    [Comment("Class for Lion")]  // Атрибут CommentAttribute для указания комментария к классу.
+    public class Lion : Animal  // Объявление класса Lion, который наследует от абстрактного класса Animal.
+    {
+        // Конструктор класса Lion.
+        public Lion(string Country, string HideFromOtherAnimal, string Name, string WhatAnimal) : base(Country, HideFromOtherAnimal, Name, WhatAnimal)
+        {
+            // Вызов конструктора базового класса Animal для инициализации свойств.
+        }
+
+        // Переопределенный метод SayHello.
+        public override void SayHello()
+        {
+            base.SayHello();  // Вызов метода базового класса.
+            Console.WriteLine(", i'm a Lion.");  // Вывод строки в консоль.
+        }
+
+        // Переопределенный метод GetFavoriteFood.
+        public override eFavoriteFood GetFavoriteFood()
+        {
+            return base.GetFavoriteFood();  // Вызов метода базового класса и возвращение его значения.
+        }
+    }
+
+    [Comment("Class for Pig")]  // Атрибут CommentAttribute для указания комментария к классу.
+    public class Pig : Animal  // Объявление класса Pig, который наследует от абстрактного класса Animal.
+    {
+        // Конструктор класса Pig.
+        public Pig(string Country, string HideFromOtherAnimal, string Name, string WhatAnimal) : base(Country, HideFromOtherAnimal, Name, WhatAnimal)
+        {
+            // Вызов конструктора базового класса Animal для инициализации свойств.
+        }
+
+        // Переопределенный метод SayHello.
+        public override void SayHello()
+        {
+            base.SayHello();  // Вызов метода базового класса.
+            Console.WriteLine(", i'm a Pig.");  // Вывод строки в консоль.
+        }
+
+        // Переопределенный метод GetFavoriteFood.
+        public override eFavoriteFood GetFavoriteFood()
+        {
+            return base.GetFavoriteFood();  // Вызов метода базового класса и возвращение его значения.
+        }
+    }
+
+    [Comment("Class for Animal")]  // Атрибут CommentAttribute для указания комментария к абстрактному классу Animal.
+    abstract public class Animal  // Объявление абстрактного класса Animal.
+    {
+        // Автоматические свойства для хранения данных о животном.
+        public string? Country { get; set; }
+        public string? HideFromOtherAnimal { get; set; }
+        public string? Name { get; set; }
+        public string? WhatAnimal { get; set; }
+        public eClassificationAnimal ClassificationAnimal { get; set; }
+        public eFavoriteFood FavoriteFood { get; set; }
+
+        // Конструктор класса Animal.
+        public Animal(string Country, string HideFromOtherAnimal, string Name, string WhatAnimal)
+        {
+            // Инициализация свойств.
+            this.Country = Country;
+            this.HideFromOtherAnimal = HideFromOtherAnimal;
+            this.Name = Name;
+            this.WhatAnimal = WhatAnimal;
+        }
+
+        // Метод Deconstruct для деконструкции объекта Animal.
+        public void Deconstruct(string Country, string HideFromOtherAnimal, string Name, string WhatAnimal)
+        {
+            Country = this.Country;
+            HideFromOtherAnimal = this.HideFromOtherAnimal;
+            Name = this.Name;
+            WhatAnimal = this.WhatAnimal;
+        }
+
+        // Свойство GetClassificationAnimal для получения классификации животного.
+        public eClassificationAnimal GetClassificationAnimal()
+        {
+            return this.ClassificationAnimal;
+        }
+
+        // Виртуальный метод GetFavoriteFood для получения любимой пищи животного.
+        public virtual eFavoriteFood GetFavoriteFood()
+        {
+            return this.FavoriteFood;
+        }
+        public virtual void SayHello()
+        {
+            Console.Write("Hello ");
+        }
+    }
+    public enum eClassificationAnimal
+    {
+        Herbivores,
+        Carnivores,
+        Omnivores
+    }
+    public enum eFavoriteFood
+    {
+        Meat,
+        Plants,
+        Everything
+    }
+}
